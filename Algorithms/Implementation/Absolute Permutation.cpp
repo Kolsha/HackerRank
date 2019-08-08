8c2df14a75bf6f2193a8cc81d7abbe6a5f0018ee
Kolsha <iam@kolsha.ru>
2019-08-08 11:48:17 +0700

    Absolute Permutation solved

---
+++ Algorithms/Implementation/Absolute Permutation.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+/*
+ * Difficulty Medium
+ * Max Score 40
+ * https://www.hackerrank.com/challenges/absolute-permutation/
+ */
+
+using namespace std;
+
+vector <string> split_string(string);
+
+bool isValid(int val, vector<bool> &used) {
+    if (val < 1 || val > used.size()) return false;
+    return !used[val];
+}
+
+// Complete the absolutePermutation function below.
+vector<int> absolutePermutation(int n, int k) {
+
+    vector<int> res;
+    vector<bool> used(n + 1, false);
+
+    // cout << "push: " ;
+
+    for (int i = 1; i <= n; i++) {
+        int val = -1;
+        /*
+         * |p[i] - i| = k
+         * 1) p[i] = i - k
+         * 2) p[i] = i + k
+         */
+        int min_v = i - k;
+        int max_v = i + k;
+        if (isValid(min_v, used)) {
+            val = min_v;
+        } else if (isValid(max_v, used)) {
+            val = max_v;
+        }
+        if (val < 1 || val > n) {
+            return vector<int>(1, -1);
+        }
+        // cout << val << ", ";
+        res.push_back(val);
+        used[val] = true;
+    }
+//    cout << endl;
+
+    return res;
+
+
+}
+
+int main() {
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string nk_temp;
+        getline(cin, nk_temp);
+
+        vector <string> nk = split_string(nk_temp);
+
+        int n = stoi(nk[0]);
+
+        int k = stoi(nk[1]);
+
+        vector<int> result = absolutePermutation(n, k);
+
+        for (int i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector <string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector <string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
