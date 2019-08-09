6ea72311739ff7bb9ad347cce807e4a8e114d9de
Kolsha <iam@kolsha.ru>
2019-08-09 10:56:36 +0700

    Is This a Binary Search Tree? solved

---
+++ Algorithms/Other/Trees: Is This a Binary Search Tree?.cpp
@@ -0,0 +1,30 @@
+/* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.
+
+The Node struct is defined as follows:
+	struct Node {
+		int data;
+		Node* left;
+		Node* right;
+	}
+*/
+/*
+ * Difficulty Medium
+ * Max Score 30
+ * https://www.hackerrank.com/challenges/ctci-is-binary-search-tree/
+ */
+bool checkBSTHelper(Node *root, int min, int max) {
+    if (!root) return true;
+
+    if (!(min < root->data && root->data < max))
+        return false;
+
+    return checkBSTHelper(root->left, min, root->data) &&
+           checkBSTHelper(root->right, root->data, max);
+
+}
+
+bool checkBST(Node *root) {
+
+    return checkBSTHelper(root, INT32_MIN, INT32_MAX);
+}
+
