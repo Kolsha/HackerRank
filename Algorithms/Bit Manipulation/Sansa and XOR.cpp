532c79524f76aad89d4420abbb31fca438ad5d03
Kolsha <iam@kolsha.ru>
2019-09-03 13:19:01 +0700

    Sansa and XOR solved

---
+++ Algorithms/Bit Manipulation/Sansa and XOR.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+/*
+ * Difficulty Medium
+ * Max Score 30
+ * https://www.hackerrank.com/challenges/sansa-and-xor/
+ */
+
+using namespace std;
+
+vector <string> split_string(string);
+
+// Complete the sansaXor function below.
+int sansaXor(vector<int> arr) {
+    int res = 0;
+    if (arr.size() % 2 == 0) {
+        return res;
+    }
+
+    for (int i = 0; i < arr.size(); i++) {
+        if (i % 2 == 0) {
+            res ^= arr[i];
+        }
+    }
+
+    return res;
+
+}
+
+int main() {
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string arr_temp_temp;
+        getline(cin, arr_temp_temp);
+
+        vector <string> arr_temp = split_string(arr_temp_temp);
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++) {
+            int arr_item = stoi(arr_temp[i]);
+
+            arr[i] = arr_item;
+        }
+
+        int result = sansaXor(arr);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector <string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector <string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
