5d4cf3811337dc4664f0ea0fd4012748d633a7aa
Kolsha <iam@kolsha.ru>
2019-09-19 11:22:38 +0700

    C++ Variadics solved

---
+++ C++/Other Concepts/C++ Variadics.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+/*
+ * Difficulty Hard
+ * Max Score 80
+ * https://www.hackerrank.com/challenges/cpp-variadics/
+ */
+// Enter your code for reversed_binary_value<bool...>()
+
+template<bool... digits>
+long reversed_binary_value() {
+    long n = 1;
+    long res = 0;
+    ((res += digits * n, n <<= 1), ...);
+    return res;
+};
+
+
+template<int n, bool...digits>
+struct CheckValues {
+    static void check(int x, int y) {
+        CheckValues<n - 1, 0, digits...>::check(x, y);
+        CheckValues<n - 1, 1, digits...>::check(x, y);
+    }
+};
+
+template<bool...digits>
+struct CheckValues<0, digits...> {
+    static void check(int x, int y) {
+        int z = reversed_binary_value<digits...>();
+        std::cout << (z + 64 * y == x);
+    }
+};
+
+int main() {
+    int t;
+    std::cin >> t;
+
+    for (int i = 0; i != t; ++i) {
+        int x, y;
+        cin >> x >> y;
+        CheckValues<6>::check(x, y);
+        cout << "\n";
+    }
+}
